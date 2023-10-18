8ef7d68d89cb55a97628dfbcdd744d6790718a8f
Marc-Daniel DALEBA <118677993+blueberry077@users.noreply.github.com>
2023-10-18 13:15:38 +0200

    Added draw_fill_triangle

--- include/term_canvas.h
+++ include/term_canvas.h
@@ -11,6 +11,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+/* Macros ................... */
+#define min(a, b)    ((a) < (b)) ? (a) : (b)
+#define max(a, b)    ((a) > (b)) ? (a) : (b)
+#define mid(a, b, c) (min(max(a, b), c))
 /* Structures ............... */
 struct Canvas {
   size_t height; // As to be a multiple of 4
@@ -39,23 +43,27 @@ struct Canvas new_canvas(size_t w, size_t h)
   };
   return ret;
 }
-
-void draw_line(struct Canvas c, size_t x1, size_t y1, size_t x2, size_t y2,
-               int t) {
+void set_pixel(struct Canvas c, size_t x, size_t y, int t)
+/* This function places a pixel *
+ * in the canvas buffer. ...... */
+{
+  if ((x > 0) && (x < c.width) && (y > 0) && (y < c.height)) {
+    c.buffer[y * c.width + x] = t;
+  }
+}
+void draw_line(struct Canvas c, size_t x1, size_t y1, size_t x2, size_t y2, int t)
+{
   int dx = abs((int)x2 - (int)x1);
   int sx = x1 < x2 ? 1 : -1;
   int dy = -abs((int)y2 - (int)y1);
   int sy = y1 < y2 ? 1 : -1;
   int err = dx + dy;
   int e2;
-
   while (1) {
     set_pixel(c, x1, y1, t);
-
-    if (x1 == x2 && y1 == y2) {
+    if ((x1 == x2) && (y1 == y2)) {
       break;
     }
-
     e2 = 2 * err;
     if (e2 >= dy) {
       err += dy;
@@ -67,14 +75,39 @@ void draw_line(struct Canvas c, size_t x1, size_t y1, size_t x2, size_t y2,
     }
   }
 }
-
-void draw_full_triangle(struct Canvas c, int x1, int y1, int x2, int y2, int x3,
-                        int y3, int t) {
+void draw_wire_frame_triangle(struct Canvas c, int x1, int y1, int x2, int y2, int x3, int y3, int t)
+{
   draw_line(c, x1, y1, x2, y2, t);
   draw_line(c, x2, y2, x3, y3, t);
   draw_line(c, x3, y3, x1, y1, t);
 }
-
+float sign(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+	return (x3 - x1) * (y3 - y2) - (x2 - x3) * (y1 - y3);
+}
+void draw_fill_triangle(struct Canvas c, int x1, int y1, int x2, int y2, int x3, int y3, int t)
+{
+  int hx = max(max(x1, x2), x3);
+  int hy = max(max(y1, y2), y3);
+  int lx = min(min(x1, x2), x3);
+  int ly = min(min(y1, y2), y3);
+  for (int _y = 0; _y < hy; ++_y) {
+    for (int _x = 0; _x < hx; ++_x) {
+      float Px = lx + _x;
+      float Py = ly + _y;
+	  
+	  float s1 = y2 - y1;
+	  float s2 = x2 - x1;
+	  float s3 = y3 - y1;
+	  float s4 = Py - y1;
+      
+	  float w1 = (x1 * s1 + s4 * s2 - Px * s1) / (s3 * s2 - (x3 - x1) * s1);
+      float w2 = (s4 - w1 * s3) / s1;
+	  if ((w1 >= 0) && (w2 >= 0) && ((w1 + w2) <= 1))
+		set_pixel(c, lx + _x, ly + _y, t);
+    }
+  }
+}
 void draw_rectangle(struct Canvas c, size_t x, size_t y, size_t w, size_t h,
                     int t)
 /* This functions draws *
@@ -100,22 +133,9 @@ void draw_circle(struct Canvas c, int x, int y, int r, int t)
     }
   }
 }
-
-void draw_triangle(struct Canvas c, size_t x, size_t y, size_t height, int t) {
-  for (size_t i = 0; i < height; i++) {
-    for (size_t j = 0; j <= i; j++) {
-      set_pixel(c, x + j, y + i, t);
-    }
-  }
-}
-
-void set_pixel(struct Canvas c, size_t x, size_t y, int t)
-/* This function places a pixel *
- * in the canvas buffer. ...... */
+void clear_canvas(struct Canvas c, int t)
 {
-  if ((x > 0) && (x < c.width) && (y > 0) && (y < c.height)) {
-    c.buffer[y * c.width + x] = t;
-  }
+	draw_rectangle(c, 0, 0, c.width, c.height, t);
 }
 void draw_canvas(struct Canvas c)
 /* This function draws the canvas buffer *
